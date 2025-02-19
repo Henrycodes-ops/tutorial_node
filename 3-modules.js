0c7e5ca460d360ea62347c618619623a349af3be
Falolu Henry <henryfalolu1@gmail.com>
2025-02-19 03:50:22 +0100

    add console log for names module; comment out sayHi function in 3-modules.js

--- 3-modules.js
+++ 3-modules.js
@@ -1,10 +1,12 @@
 // Modules
 // Modules are reusable pieces of code that can be exported from one program and imported for use in another program.
 const names = require('./names') 
+console.log(names);
 
 
-const sayHi = (name) => {
-  console.log(`Hello there ${name}`);
-}
 
-sayHi('Susan');
+// const sayHi = (name) => {
+//   console.log(`Hello there ${name}`);
+// }
+
+// sayHi('Susan');
