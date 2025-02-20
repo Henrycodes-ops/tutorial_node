502f8f417b23afb2b6bb0bf5a730e81e1fd55298
Falolu Henry <henryfalolu1@gmail.com>
2025-02-20 09:36:18 +0100

    refactor file system operations: add synchronous logging and migrate async logic to new file

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,4 +1,6 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs"); 
+console.log('start');
+
 
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
@@ -9,5 +11,8 @@ writeFileSync(
   `Here is the result: ${first}, ${second}`,
   { flag: "a" }
 );
+
+console.log('Done with this task');
+
 
 
