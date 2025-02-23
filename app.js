7a82926e8b15fcd77546d740da9b3706d8bbf57d
Falolu Henry <henryfalolu1@gmail.com>
2025-02-23 19:46:03 +0100

    learning about event loop and the it executes

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const _ = require('lodash')
+// const _ = require('lodash')
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
-console.log('Hello World');
+// const items = [1, [2, [3, [4]]]]
+// const newItems = _.flattenDeep(items)
+// console.log(newItems);
+
+console.log("first task");
+console.time();
+
+// for (let i = 0; i < 1000000; i++) {
+//   const h3 = document.querySelector('h3')
+//   h3.textContent = `Hey, everyone is waiting on me`
+// }
+
+setTimeout(() => {
+  console.log("second task");
+}, 0);
+
+console.timeEnd();
+
+console.log("next task");
 
