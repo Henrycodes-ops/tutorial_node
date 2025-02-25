28c3ba904f013034bfd321d18d8de93027663762
Falolu Henry <henryfalolu1@gmail.com>
2025-02-25 12:27:30 +0100

    Implement HTTP server with routing and add file reading functionality

--- 1-event-loop-examples/server.js
+++ 1-event-loop-examples/server.js
@@ -1,1 +1,23 @@
-const http = require('http')
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log("request event");
+  // res.end('Hello world')
+  if (req.url === "/") {
+    // // BLOCKING CODE
+    // for (let i = 0; i < 1000; i++) {
+    //   for (let j = 0; j < 1000; j++) {
+    //     console.log(`${i} ${j}`);
+
+    //   }
+    // }
+    res.end("Home Page");
+  }
+  if (req.url === "/about") {
+    res.end("About Page");
+  }
+  res.end("Error Page");
+});
+
+server.listen(5000, () => {
+  console.log("Server listening on port 5000.....");
+});
