cb845e9d8b5559a3ca804eb24247e1dd1004d6bd
Falolu Henry <henryfalolu1@gmail.com>
2025-02-25 14:41:09 +0100

    Refactor app.js to use EventEmitter and remove unused file handling code; add new promises example

--- app.js
+++ app.js
@@ -1,38 +1,10 @@
-import { promises } from "fs";
-const { readFile, writeFile } = promises;
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+import EventEmitter from 'events'
 
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-mind-grenade.txt",
-      `This is just awesome : ${first} ${second}`, 
-      {flag: 'a'}
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.error(error);
-  }
-};
+const customEmitter = new EventEmitter()
 
-start();
+customEmitter.on('response', () => {
+  console.log(`data recieved `);
+  
+})
 
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
+customEmitter.emit('response')
