f8641aec5f068dbc3d42dc7700e75ac9be26443e
Falolu Henry <henryfalolu1@gmail.com>
2025-02-26 16:09:56 +0100

    Add file creation script and refactor event emitter examples; implement HTTP server

---
+++ 14-event-emmiter.js
@@ -0,0 +1,12 @@
+import EventEmitter from "events";
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on("response", (name, id) => {
+  console.log(`data recieved user ${name} with id:${id} `);
+});
+customEmitter.on("response", () => {
+  console.log(`some other logic here `);
+});
+
+customEmitter.emit("response", "john", 34);
