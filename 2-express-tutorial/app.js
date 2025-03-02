88b1846bd684831bbe210eb87f5191b196fb28a9
Falolu Henry <henryfalolu1@gmail.com>
2025-03-02 08:49:08 +0100

    Enhance HTTP server: add routing for home and about pages, and implement 404 error handling

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,15 +1,32 @@
-console.log("express tutorial");
+console.log("Express tutorial");
 import http from "http";
 
 const server = http.createServer((req, res) => {
   // res.writeHead(200, {'content-type': 'text/html'})
   // res.end('<h1>Home page</h1>')
   // better one is
-  console.log(req);
+  const url = req.url 
 
-  res.writeHead(200, { "content-type": "text/html" });
+  if (url === '/') {
+    res.writeHead(200, { "content-type": "text/html" });
   res.write("<h1>Home page</h1>", "utf8");
   res.end();
+  }
+  // about
+  else if (url === '/about') {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>About page</h1>", "utf8");
+      res.end();
+  }
+
+  // 404
+  else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>page not found</h1>", "utf8");
+      res.end();
+  }
+
+  
 });
 
 server.listen(5000);
