1dbfea7535a5cdd0a64423cf0eefd7f2f4be2fa3
Falolu Henry <henryfalolu1@gmail.com>
2025-03-03 16:25:36 +0100

    Fix error handling for index.html and update server response for home page

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,36 +1,42 @@
-console.log("Express tutorial");
 import http from "http";
 import { readFileSync } from "fs";
 
-// get all files
-const homePage = readFileSync('./index.html')
+console.log("Express tutorial");
+
+let homePage;
+try {
+  homePage = readFileSync("./index.html");
+} catch (err) {
+  console.error("Error reading index.html:", err);
+  homePage = "<h1>Error loading page</h1>";
+}
 
 const server = http.createServer((req, res) => {
   // res.writeHead(200, {'content-type': 'text/html'})
   // res.end('<h1>Home page</h1>')
   // better one is
-  const url = req.url 
+  const url = req.url;
 
-  if (url === '/') {
-    res.writeHead(200, { "content-type": "text/html" });
-  res.write('homePage');
-  res.end();
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.write(homePage);
+    res.end();
   }
   // about
-  else if (url === '/about') {
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write("<h1>About page</h1>", "utf8");
-      res.end();
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About page</h1>");
   }
 
   // 404
   else {
-      res.writeHead(404, { "content-type": "text/html" });
-      res.write("<h1>page not found</h1>", "utf8");
-      res.end();
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
   }
-
-  
 });
 
-server.listen(5000);
+// The server is listening on port 5000
+// because it is a commonly used port for development purposes.
+server.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
