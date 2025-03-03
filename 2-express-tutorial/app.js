5ad5d101794437540a00cfba5511575f0243e8ec
Falolu Henry <henryfalolu1@gmail.com>
2025-03-03 17:04:32 +0100

    Add comments to app.js explaining Express methods and start server on port 4000

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,3 +1,20 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
+
+// Methods in express
+// app.get = read data
+// app.post = create data
+// app.put = update data
+// app.delete = delete data
+// app.all = all methods
+// app.use = middleware
+// app.listen = start server
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home page</h1>");
+});
+
+app.listen(4000, () => {
+  console.log("Server is listening on http://localhost:4000");
+});
