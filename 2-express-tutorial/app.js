4694050c6961f79c734e1ff692c95abb7576ffe7
Falolu Henry <henryfalolu1@gmail.com>
2025-03-03 17:06:13 +0100

    Add about page route and 404 error handling in app.js

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -15,6 +15,14 @@ app.get("/", (req, res) => {
   res.send("<h1>Home page</h1>");
 });
 
+app.get("/about", (req, res) => { 
+  res.send("<h1>About page</h1>");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Page not found</h1>");
+}
+
 app.listen(4000, () => {
   console.log("Server is listening on http://localhost:4000");
 });
