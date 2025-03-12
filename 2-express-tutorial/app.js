89ee787cf22aaf14eeee57d03f4e888883a18338
Falolu Henry <henryfalolu1@gmail.com>
2025-03-12 11:43:40 +0100

    Refactor app.js to use CommonJS syntax and set up a new route for products

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,32 +1,13 @@
-import express from "express";
-import path from 'path'
+const express = require('express')
+const app = express()
+const { products } = require('./data');
 
 
-const app = express();
+app.get('/', (req, res) => {
+  res.json(products)
+})
 
-// Methods in express
-// app.get = read data
-// app.post = create data
-// app.put = update data
-// app.delete = delete data
-// app.all = all methods
-// app.use = middleware
-// app.listen = start server
-
-
-// Setup static and middleware
-app.use(express.static('./navbar-app'))
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve('./navbar-app/index.html'));
-});
-
-
-
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Page not found</h1>");
-});
-
-app.listen(4000, () => {
-  console.log("Server is listening on http://localhost:4000");
-});
+app.listen(3000, () => {
+  console.log('server is listening to port 5000');
+  
+})
